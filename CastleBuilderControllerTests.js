f1debe43564e36d930c6bf687f3a6a651a450e23
Nandez89 <dahernandez.89@gmail.com>
2017-10-03 14:41:37 -0700

    Format changes

--- CastleBuilderControllerTests.js
+++ CastleBuilderControllerTests.js
@@ -1,63 +1,63 @@
 'use strict';
 
-describe('Castle Company Test suit', function() {
+describe('Castle Company Test suit', function () {
 
-  describe('Castle Builder Controller Tests', function() {
-    var $scope;
+    describe('Castle Builder Controller Tests', function () {
+        var $scope;
 
-    beforeEach(module('CastleCompany'));
+        beforeEach(module('CastleCompany'));
 
-    beforeEach(inject(function($rootScope, $controller) {
-      $scope = $rootScope.$new();
-      $controller('CastleBuilderController', {
-        $scope: $scope
-      });
-    }));
+        beforeEach(inject(function ($rootScope, $controller) {
+            $scope = $rootScope.$new();
+            $controller('CastleBuilderController', {
+                $scope: $scope
+            });
+        }));
 
-    it('should return the array without consecutive duplicate values', function() {
-      var array = [1,2,2,3,4,4];
-      expect($scope.removeConsecutiveDuplicates(array)).toEqual([1,2,3,4]);
+        it('should return the array without consecutive duplicate values', function () {
+            var array = [1, 2, 2, 3, 4, 4];
+            expect($scope.removeConsecutiveDuplicates(array)).toEqual([1, 2, 3, 4]);
+        });
+
+        describe('Build Castles', function () {
+
+            it('should set castles to 0 if no data is defined', function () {
+                $scope.buildCastles();
+
+                expect($scope.castles).toEqual(0);
+            });
+
+            it('should set castles to 1 if the input array is length 1', function () {
+                $scope.landHeights = [999];
+                $scope.buildCastles();
+
+                expect($scope.castles).toEqual(1);
+            });
+
+            it('should set castles to 2 if the input array has only one peak or valley', function () {
+                $scope.landHeights = [1, 2, 1];
+                $scope.buildCastles();
+
+                expect($scope.castles).toEqual(2);
+
+                $scope.landHeights = [2, 1, 2];
+                $scope.buildCastles();
+
+                expect($scope.castles).toEqual(2);
+            });
+
+            it('should only count a peak or a valley once when its represented by consecutive equal integers', function () {
+                $scope.landHeights = [1, 2, 2, 2, 2, 1];
+                $scope.buildCastles();
+
+                expect($scope.castles).toEqual(2);
+
+                $scope.landHeights = [2, 1, 1, 1, 1, 2];
+                $scope.buildCastles();
+
+                expect($scope.castles).toEqual(2);
+            });
+        });
+
     });
-    
-    describe('Build Castles', function() {
-      
-      it('should set castles to 0 if no data is defined', function() {
-        $scope.buildCastles();
-        
-        expect($scope.castles).toEqual(0);
-      });
-      
-      it('should set castles to 1 if the input array is length 1', function() {
-        $scope.landHeights = [999];
-        $scope.buildCastles();
-        
-        expect($scope.castles).toEqual(1);
-      });
-      
-      it('should set castles to 2 if the input array has only one peak or valley', function() {
-        $scope.landHeights = [1,2,1];
-        $scope.buildCastles();
-        
-        expect($scope.castles).toEqual(2);
-        
-        $scope.landHeights = [2,1,2];
-        $scope.buildCastles();
-        
-        expect($scope.castles).toEqual(2);
-      });
-      
-      it('should only count a peak or a valley once when its represented by consecutive equal integers', function() {
-        $scope.landHeights = [1,2,2,2,2,1];
-        $scope.buildCastles();
-        
-        expect($scope.castles).toEqual(2);
-        
-        $scope.landHeights = [2,1,1,1,1,2];
-        $scope.buildCastles();
-        
-        expect($scope.castles).toEqual(2);
-      });
-    });
-    
-  });
 });
--- app.js
+++ app.js
@@ -1,61 +1,42 @@
 var app = angular.module('CastleCompany', ['ngMockE2E']);
 
-app.controller('CastleBuilderController', function($scope) {
+app.controller('CastleBuilderController', function ($scope) {
 
-  $scope.landHeights = [];
+    $scope.landHeights = [];
 
-  $scope.removeConsecutiveDuplicates = function(array) {
+    $scope.removeConsecutiveDuplicates = function (array) {
+        return array.filter(function (value, position, array) {
+            var previousValue = array[position - 1];
+            return position === 0 || value !== previousValue;
+        });
+    };
 
+    function countBuildingPlaces(landHeights) {
 
+        var buildingPlaces = 0;
 
+        if (landHeights.length > 0) {
 
-    return array.filter(function(value, position, array) {
-      var previousValue = array[position - 1];
-      return position === 0 || value !== previousValue;
-    });
+            buildingPlaces++;
 
+            for (i = 1; i < landHeights.length; i++) {
 
+                var isPeak = landHeights[i] > landHeights[i - 1] && landHeights[i] > landHeights[i + 1];
+                var isValley = landHeights[i] < landHeights[i - 1] && landHeights[i] < landHeights[i + 1];
 
+                if (isPeak || isValley) {
+                    buildingPlaces++;
+                }
+            }
 
-  };
-
-  function countBuildingPlaces(landHeights) {
-
-    var buildingPlaces = 0;
-
-    if (landHeights.length > 0) {
-
-      buildingPlaces++;
-
-      for (i = 1; i < landHeights.length; i++) {
-
-        var isPeak = landHeights[i] > landHeights[i - 1] && landHeights[i] > landHeights[i + 1];
-        var isValley = landHeights[i] < landHeights[i - 1] && landHeights[i] < landHeights[i + 1];
-
-        if (isPeak || isValley) {
-          buildingPlaces++;
         }
-      }
-
-
-
-
+        return buildingPlaces;
     }
-    return buildingPlaces;
-  }
 
+    $scope.buildCastles = function () {
+        $scope.landHeights = $scope.landHeights.map(Number);
+        $scope.landHeights = $scope.removeConsecutiveDuplicates($scope.landHeights);
 
-
-
-
-  $scope.buildCastles = function() {
-    $scope.landHeights = $scope.landHeights.map(Number);
-    $scope.landHeights = $scope.removeConsecutiveDuplicates($scope.landHeights);
-
-
-
-
-
-    $scope.castles = countBuildingPlaces($scope.landHeights);
-  };
+        $scope.castles = countBuildingPlaces($scope.landHeights);
+    };
 });
